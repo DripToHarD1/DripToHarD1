e9bae32531067db2b8b0d4373dad60f3fa36e0ef
DripToHarD1 <116714497+DripToHarD1@users.noreply.github.com>
2022-12-07 14:45:47 +0700

    Update Work1.js

--- Week6/Work1.js
+++ Week6/Work1.js
@@ -3,7 +3,7 @@ let order =
     {
     "name" : ["Mr.Lee","Mr.Yue","Panpong"],
     "Itemorder" : ["A4 Paper","Ruler","Flash Drive"],
-    "Price" : [24,12.5,65,40],
+    "Price" : [120,25,130],
     "Qty" : [5,2,3]
     }
     console.log(order.name[0],order.Itemorder[0],order.Price[0]*order.Qty[0])
@@ -12,8 +12,8 @@ let order =
     console.log("   Result  ",Result0)
 
     console.log(order.name[1],order.Itemorder[2],order.Price[2]*order.Qty[1])
-    console.log("         ",order.Itemorder[0],order.Price[3]*order.Qty[2])
-   let Result1 = (order.Price[2]*order.Qty[1])+(order.Price[3]*order.Qty[2])
+    console.log("         ",order.Itemorder[0],order.Price[1]*order.Qty[2])
+   let Result1 = (order.Price[2]*order.Qty[1])+(order.Price[1]*order.Qty[2])
     console.log("   Result  ",Result1) 
 
     console.log(order.name[2],order.Itemorder[0],order.Price[1]*order.Qty[2])
